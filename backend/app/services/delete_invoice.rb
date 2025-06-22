12c2a29d1459486e0364c739d978272b8dfc722b
Jeremy Osih <osih.jeremy@gmail.com>
2025-06-22 18:20:12 +0200

    fix: linting errors

--- backend/app/services/delete_invoice.rb
+++ backend/app/services/delete_invoice.rb
@@ -1,28 +1,28 @@
 # frozen_string_literal: true
 
 class DeleteInvoice
-    INVOICE_STATUSES_THAT_DENY_DELETION = Invoice::PAID_OR_PAYING_STATES + [Invoice::FAILED, Invoice::PROCESSING]
+  INVOICE_STATUSES_THAT_DENY_DELETION = Invoice::PAID_OR_PAYING_STATES + [Invoice::FAILED, Invoice::PROCESSING]
 
-    def initialize(invoice:, deleted_by:)
-      @invoice = invoice
-      @deleted_by = deleted_by
+  def initialize(invoice:, deleted_by:)
+    @invoice = invoice
+    @deleted_by = deleted_by
+  end
+
+  def perform
+    invoice.with_lock do
+      return unless can_delete?
+
+      # Clean up related records
+      invoice.invoice_approvals.destroy_all
+      invoice.invoice_line_items.destroy_all
+      invoice.invoice_expenses.destroy_all
+
+      # Remove from QuickBooks if integrated
+      invoice.integration_records.each(&:mark_deleted!)
+
+      invoice.destroy!
     end
-
-    def perform
-      invoice.with_lock do
-        return unless can_delete?
-
-        # Clean up related records
-        invoice.invoice_approvals.destroy_all
-        invoice.invoice_line_items.destroy_all
-        invoice.invoice_expenses.destroy_all
-
-        # Remove from QuickBooks if integrated
-        invoice.integration_records.each(&:mark_deleted!)
-
-        invoice.destroy!
-      end
-    end
+  end
 
     private
       attr_reader :invoice, :deleted_by
@@ -30,4 +30,4 @@ class DeleteInvoice
       def can_delete?
         !invoice.status.in?(INVOICE_STATUSES_THAT_DENY_DELETION)
       end
-  end
+end
--- backend/app/services/delete_many_invoices.rb
+++ backend/app/services/delete_many_invoices.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 class DeleteManyInvoices
-    def initialize(company:, deleted_by:, invoice_ids:)
-      @company = company
-      @deleted_by = deleted_by
-      @invoice_ids = invoice_ids
-    end
+  def initialize(company:, deleted_by:, invoice_ids:)
+    @company = company
+    @deleted_by = deleted_by
+    @invoice_ids = invoice_ids
+  end
 
-    def perform
-      invoices = company.invoices.where(external_id: invoice_ids)
-      raise ActiveRecord::RecordNotFound if invoices.size != invoice_ids.size
+  def perform
+    invoices = company.invoices.where(external_id: invoice_ids)
+    raise ActiveRecord::RecordNotFound if invoices.size != invoice_ids.size
 
-      invoices.each do |invoice|
-        DeleteInvoice.new(invoice:, deleted_by:).perform
-      end
+    invoices.each do |invoice|
+      DeleteInvoice.new(invoice:, deleted_by:).perform
     end
+  end
 
     private
       attr_reader :company, :deleted_by, :invoice_ids
-  end
+end
